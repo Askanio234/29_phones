aa7b39beed000dab03b0b7d7699319046ea5c0c9
Artem Chernyaev <askanio234@gmail.com>
2017-07-18 18:05:05 +0300

    renamed env variable for db connection to DB_URI

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-db_configuration = os.getenv("db_conf")
+db_configuration = os.getenv("DB_URI")
 if not db_configuration:
     raise Exception('db connection info should be '
         'specified as env variable "db_conf"')
